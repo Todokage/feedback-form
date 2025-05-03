df32b4e38388d00eacfee9c42d948f3115cf50a6
Todokage <ftdraco176@gmail.com>
2025-05-03 18:07:27 +0300

    create:project

---
+++ assignment/src/App.js
@@ -0,0 +1,17 @@
+
+import Navbar from './components/navbar';
+import Body from './components/body';
+import Footer from './components/footer';
+
+import './component.css';
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
